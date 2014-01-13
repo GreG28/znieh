afdcc095e2a6ff685a68d7d65b169e5a38943449
spyl94 <spyl94@gmail.com>
2014-01-13 11:54:30 +0100

    add armor loaders

---
+++ src/Znieh/VillageGameBundle/DataFixtures/ORM/LoadArmorPartTypeData.php
@@ -0,0 +1,49 @@
+<?php
+namespace Znieh\VillageGameBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture,
+    Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Znieh\VillageGameBundle\Entity\ArmorPartType;
+use Znieh\PublicBundle\DataFixtures\ORM\AbstractFixtureLoader;
+
+class LoadArmorPartTypeData extends AbstractFixtureLoader implements OrderedFixtureInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+      $armorPartTypesData = $this->getModelFixtures();
+
+        // Create weaponPartTypes
+        foreach($armorPartTypesData as $armorPartTypeData) {
+            $armorPartType = new ArmorPartType();
+
+            $armorPartType
+                ->setName($armorPartTypeData['name'])
+            ;
+
+            $manager->persist($armorPartType);
+            $manager->flush();
+        }
+    }
+
+    /**
+     * The main fixtures file for this loader.
+     */
+    public function getModelFile()
+    {
+        return 'armorPartType';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 26;
+    }
+}
