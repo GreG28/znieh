4839525479a824a2c9443b6b5042944737d8529b
Florian <florian.lepot@gmail.com>
2014-01-18 18:53:12 +0100

    Fix and clear units creation

--- web/js/game/contentManager.js
+++ web/js/game/contentManager.js
@@ -1,10 +1,20 @@
 var unistJson;
-var taille_sprite;
 
 function ContentManager(stage, width, height) {
 
-    this.init = function () {
+    var unitsCaracteristics = {
+        PETITFIN: "petitfin",
+        PETITMOYEN: "petitmoyen",
+        PETITMUSCLE: "petitmuscle",
+        NORMALFIN: "normalfin",
+        NORMALMOYEN: "normalmoyen",
+        NORMALMUSCLE: "normalmuscle",
+        GRANDFIN: "grandfin",
+        GRANDMOYEN: "grandmoyen",
+        GRANDMUSCLE: "grandmuscle",
+    }
 
+    this.init = function () {
         loadingQueue = new createjs.LoadQueue(false);
         loadingQueue.addEventListener("complete", initMap);
         loadingQueue.loadManifest([{id:"tileset", src:"../../img/sprites/spritemap.png"}]); // On oblige le chargement de l'image avant l'exécution de la suite, sinon la map n'est pas chargée avant le stage.update()
@@ -15,9 +25,6 @@ function ContentManager(stage, width, height) {
         loadingQueue.loadManifest([{id:"units-json", src:"../../json/units.json"}]);
         loadingQueue.loadManifest([{id:"mailarmor", src:"../../img/sprites/mailarmor.png"}]);
         loadingQueue.loadManifest([{id:"mailarmor2", src:"../../img/sprites/mailarmor2.png"}]);
-
-        // Should it load every sprites for every possibilities for units ?
-        taille_sprite = "petit";
     };
 
     function initMap() {
@@ -39,35 +46,26 @@ function ContentManager(stage, width, height) {
 
         unistJson = jQuery.parseJSON(loadingQueue.getResult("units-json",true));
 
-        // try to create units from units.json
-        createUnitFromJson(5,3,"firefox");
-        createUnitFromJson(5,4,"firefox");
-        createUnitFromJson(5,5,"firefox");
-        createUnitFromJson(7,5,"mailarmor");
-        createUnitFromJson(7,6,"mailarmor");
-        createUnitFromJson(7,7,"mailarmor");
+        createUnit(5,3,"firefox", unitsCaracteristics.PETITFIN);
+        createUnit(5,4,"firefox", unitsCaracteristics.PETITFIN);
+        createUnit(5,5,"firefox", unitsCaracteristics.PETITFIN);
+        createUnit(7,5,"mailarmor", unitsCaracteristics.PETITFIN);
+        createUnit(7,6,"mailarmor", unitsCaracteristics.PETITFIN);
+        createUnit(7,7,"mailarmor", unitsCaracteristics.PETITFIN);
 
-        taille_sprite = "grand";
-
-        createUnitFromJson(9,3,"firefox");
-        createUnitFromJson(9,4,"firefox");
-        createUnitFromJson(9,5,"firefox");
-        createUnitFromJson(2,5,"mailarmor");
-        createUnitFromJson(2,6,"mailarmor");
-        createUnitFromJson(2,7,"mailarmor");
+        createUnit(9,3,"firefox", unitsCaracteristics.GRANDMUSCLE);
+        createUnit(9,4,"firefox", unitsCaracteristics.GRANDMUSCLE);
+        createUnit(9,5,"firefox", unitsCaracteristics.GRANDMUSCLE);
+        createUnit(3,5,"mailarmor", unitsCaracteristics.GRANDMUSCLE);
+        createUnit(3,6,"mailarmor", unitsCaracteristics.GRANDMUSCLE);
+        createUnit(3,7,"mailarmor", unitsCaracteristics.GRANDMUSCLE);
 
     }
 
-    function createUnit(x, y, type) {
-        spritePerso = loadingQueue.getResult("unit" + type);
-        this.Start = this.map.GetBounds(x, y).GetBottomCenter();
-        this.Hero = new Unit(spritePerso, this.map, this.Start);
-    }
-
-    function createUnitFromJson(x, y, type) {
-        var loading_id = unistJson[type].specifications[taille_sprite].sprites.spritesheet_loading_ID;
+    function createUnit(x, y, type, taille) {
+        var loading_id = unistJson[type].specifications[taille].sprites.spritesheet_loading_ID;
         spritePerso = loadingQueue.getResult(loading_id);
         this.Start = this.map.GetBounds(x, y).GetBottomCenter();
-        this.Hero = new UnitJson(spritePerso, this.map, this.Start,unistJson[type]);
+        this.Hero = new Unit(spritePerso, this.map, this.Start, unistJson[type], taille);
     }
 }
