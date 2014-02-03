5c5d371310fdb994f62bce90b717ede825bbb21a
GreG28 <gregoire.fouillard@gmail.com>
2014-02-03 18:01:33 +0100

    Adding of a variable containing the unit selected #22

--- web/js/game/Unit.js
+++ web/js/game/Unit.js
@@ -161,6 +161,7 @@
 
         var i = this._i;
         var j = this._j;
+        var that = this;
 
         this._container.on("click", function(evt) {
             console.log("[UNIT] x" + container.x + " y" + container.y);
@@ -168,6 +169,8 @@
             
             ContentManager.DeselectTilesAndUnits();
             ContentManager.selectTiles(i,j);
+            selected_Unit = that;
+
         });
 
         this._container.x = position.x;
