5618844ab2ee4f12964ab04a3fcefc25fb409395
GreG28 <gregoire.fouillard@gmail.com>
2014-02-03 18:01:33 +0100

    Adding of a variable containing the unit selected #22

--- web/js/game/contentManager.js
+++ web/js/game/contentManager.js
@@ -14,6 +14,7 @@ var unitsToMove = [];
 var substage;
 
 var placement_en_cours = true;
+var selected_Unit = null;
 
 function ContentManager(stage, width, height) {
 
@@ -183,6 +184,8 @@ function ContentManager(stage, width, height) {
         {
             ContentManager.units[cpt3].shape_selected.visible = false;
         }
+
+        selected_Unit = null;
     };
 
     function addUnitImageInMenu()
