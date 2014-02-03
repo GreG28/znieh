b23885d12e9ddfa6d3d337c685306fc47e1958b7
Florian <florian.lepot@gmail.com>
2014-02-03 22:24:53 +0100

    Clean code and add comments

--- web/js/game/Unit.js
+++ web/js/game/Unit.js
@@ -28,6 +28,16 @@
     Unit.prototype.IsAlive = true;
     Unit.prototype.IsOnGround = true;
 
+    /**
+     * Initialize the Unit with its animations
+     * @param  {Image} imgUnit
+     * @param  {Map} map
+     * @param  {position} position
+     * @param  {Array} unitsInfos
+     * @param  {int} taille
+     * @param  {int} i
+     * @param  {int} j
+     */
     Unit.prototype.initialize = function (imgUnit, map, position, unitsInfos, taille, i, j) {
 
         "use strict";
@@ -107,7 +117,7 @@
 
     /**
      * Reset an Unit, set proprierties and handle clicks
-     * @param {Position} position
+     * @param {[type]} position
      */
     Unit.prototype.Reset = function (position) {
 
@@ -166,7 +176,7 @@
         this._container.on("click", function(evt) {
             console.log("[UNIT] x" + container.x + " y" + container.y);
             /* On rend toutes les cases autours selectionnés */
-            
+
             ContentManager.DeselectTilesAndUnits();
             ContentManager.selectTiles(i,j);
             selected_Unit = that;
@@ -182,141 +192,6 @@
         //stage.addChild(this._container);
         substage.addChild(this._container);
     };
-
-    Unit.prototype.move = function (x, y) {
-
-        var origin_x = this._i
-        var origin_y = this._j;
-
-        var limit = 7; // TODO : Sélectionner la limite de déplacement de l'unité
-        var easystar = new EasyStar.js();
-        var acceptableTiles = [ 1, 2];
-        easystar.setGrid(map.textTiles);
-        easystar.setAcceptableTiles(acceptableTiles);
-
-        unitsPlacement.length = 0;
-        for (var t = ContentManager.units.length - 1; t >= 0; t--) {
-            unitsPlacement.push([ContentManager.units[t]._i, ContentManager.units[t]._j]);
-            easystar.avoidAdditionalPoint(ContentManager.units[t]._i, ContentManager.units[t]._j);
-        }
-
-        self = this;
-        easystar.findPath(self._i, self._j, x, y, function(path) {
-            if(path.length <= limit) {
-                var filtered_new = $(unitsPlacement).filter(function(){
-                    return x == this[0] && y == this[1];
-                });
-
-                if(filtered_new.length == 0) {
-                    self._container.x = map.GetBounds(x, y).GetBottomCenter().x;
-                    self._container.y = map.GetBounds(x, y).GetBottomCenter().y;
-                    self._i = x;""
-                    self._j = y;
-
-                    self.shape_hover.graphics.drawRect((-16), (-16), 32, 32 - 2); // Change size as-needed
-                    self.shape_selected.graphics.drawRect((-16), (-16), 32 - 2, 32 - 2); // Change size as-needed
-                    self.shape_selected_unit.graphics.drawRect((-16), (-16), 32 - 2, 32 - 2); // Change size as-needed
-
-                    self._container.removeAllChildren();
-                    self._container.addChild(self.shape_hover);
-                    self._container.addChild(self.shape_selected);
-                    self._container.addChild(self.shape_selected_unit);
-                    self._container.addChild(self.sprite_base);
-
-                    var placement = [origin_x, origin_y];
-                    var filtered = $(unitsPlacement).filter(function(){
-                        return placement[0] == this[0] && placement[1] == this[1];
-                    });
-
-                    if(filtered.length > 0){
-                        map.tiles[origin_y][origin_x].shape_hover.visible = false; // Change size as-needed
-                        map.tiles[origin_y][origin_x].shape_selection_possible.visible = false; // Change size as-needed
-                        map.tiles[origin_y][origin_x].shape_selection_impossible.visible = false; // Change size as-needed
-                    }
-
-                    self.sprite_base.gotoAndPlay("move-left"); //animate
-                    ContentManager.units[self.unitID - units.length - 1] = self;
-                }
-                else console.log("Une unité est déjà sur la case.");
-            }
-            else console.log("Cette unité ne peut pas se déplacer aussi loin.")
-        });
-        easystar.calculate();
-
-        ContentManager.unSelectAllTiles();
-        ContentManager.clearUnitsMenu();
-        setEnnemySide();
-    }
-
-    Unit.prototype.getAllTilesStatut = function () {
-        if(gameStatut != GameStatut.PLACEMENT) {
-            ContentManager.clearUnitsMenu();
-        }
-
-        var limit = 7; // TODO : Sélectionner la limite de déplacement de l'unité
-        var easystar = new EasyStar.js();
-        var acceptableTiles = [ 1, 2];
-        easystar.setGrid(map.textTiles);
-        easystar.setAcceptableTiles(acceptableTiles);
-
-        unitsPlacement.length = 0;
-        for (var t = ContentManager.units.length - 1; t >= 0; t--) {
-            unitsPlacement.push([ContentManager.units[t]._i, ContentManager.units[t]._j]);
-            easystar.avoidAdditionalPoint(ContentManager.units[t]._i, ContentManager.units[t]._j);
-        }
-
-        if(gameStatut == GameStatut.IDLE || gameStatut == GameStatut.MOVE) {
-            $("#unit-" + (this.unitID - units.length - 1)).addClass("selected");
-
-            for (var x = 0; x < map.textTiles.length; x++) {
-                for (var y = 0; y < map.textTiles[0].length; y++) {
-                    //console.log("[x" + i + ", y" + j + "] - [x" + x + ", y" + y + "]");
-
-                    var self = this;
-                    easystar.findPath(this._i, this._j, x, y, function(path) {
-                        var shape = null;
-                        if (path === null) {
-                            shape = map.tiles[y][x].shape_selection_impossible;
-                            shape.visible = true;
-                        } else {
-                            if(path.length <= limit) {
-                                var placement = [x, y];
-                                var filtered = $(unitsPlacement).filter(function(){
-                                    return placement[0] == this[0] && placement[1] == this[1];
-                                });
-
-                                if(filtered.length > 0){
-                                    shape = map.tiles[y][x].shape_selection_impossible;
-                                    shape.visible = true;
-                                }
-                                else {
-                                    shape = map.tiles[y][x].shape_selection_possible;
-                                    shape.visible = true;
-                                }
-
-                                shape = self.shape_selected_unit;
-                                shape.visible = true;
-                            }
-                            else{
-                                shape = map.tiles[y][x].shape_selection_impossible;
-                                shape.visible = true;
-                            }
-                        }
-                    });
-                    easystar.calculate();
-                }
-            }
-        }
-
-        if(gameStatut == GameStatut.IDLE) {
-            gameStatut = GameStatut.MOVE;
-        }
-        else {
-            gameStatut = GameStatut.IDLE;
-        }
-
-
-    }
 
     /**
      * Gets a rectangle around the Unit
