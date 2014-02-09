2f0a6d6981e878c9a6b4ff6a52735587febcbc17
spyl94 <spyl94@gmail.com>
2014-02-09 15:36:16 +0100

    fix loader

--- src/Znieh/VillageGameBundle/DataFixtures/ORM/LoadWeaponPartData.php
+++ src/Znieh/VillageGameBundle/DataFixtures/ORM/LoadWeaponPartData.php
@@ -22,6 +22,7 @@ class LoadWeaponPartData extends AbstractFixtureLoader implements OrderedFixture
         foreach($weaponPartsData as $weaponPartData) {
             $weaponPart = new weaponPart();
 
+            //echo $weaponPartData['name'];
             $type = $manager->getRepository('ZniehVillageGameBundle:WeaponPartType')->findOneByName($weaponPartData['type']);
             $step = $manager->getRepository('ZniehVillageGameBundle:Step')->findOneByTitle($weaponPartData['step']);
 
