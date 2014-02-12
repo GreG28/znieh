138997ffb14816147dfd41576944baa905bfeb5f
Naixela <alexianfm@gmail.com>
2014-02-12 18:27:46 +0100

    first unit class created

---
+++ gameserver/unitHandler.js
@@ -0,0 +1,24 @@
+var UnitHandler = {};
+module.exports = UnitHandler;
+
+UnitHandler.StatSet = function (life, penetration, precision, evade, parry, defense, armor, strength, agility, intelligence, magicDamage, evilScience, magicSupport){
+	this.life = life;
+	this.penetration = penetration;
+	this.precision = precision;
+	this.evade = evade;
+	this.parry = parry;
+	this.defense = defense;
+	this.armor = armor;
+	this.strength = strength;
+	this.agility = agility;
+	this.intelligence = intelligence;
+	this.magicDamage = magicDamage;
+	this.evilScience = evilScience;
+	this.magicSupport = magicSupport;
+}
+
+UnitHandler.Weapon = function (name, damages, attribute, isRanged, stats)
+
+UnitHandler.Unit = function(name, sign, stats){
+
+}
