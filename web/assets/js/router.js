02d5dd5ce9c865d8818cb9336e3ab5a794632b82
spyl94 <spyl94@gmail.com>
2014-03-04 16:48:49 +0100

    add router.js support

---
+++ web/assets/js/router.js
@@ -0,0 +1,5 @@
+// backendRouter.js
+define(['fos_routing'], function (router) {
+    fos.Router.setData({"base_url":"","routes":{"znieh_villagegame_public_unlock":{"tokens":[["variable","\/","[^\/]++","object"],["text","\/village\/unlock"]],"defaults":[],"requirements":[],"hosttokens":[]}},"prefix":"","host":"localhost","scheme":"http"});
+    return router;
+});
