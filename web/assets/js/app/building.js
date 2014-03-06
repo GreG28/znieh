36a6c54df41e1c3d1ac26abb57abfaf62256da25
spyl94 <spyl94@gmail.com>
2014-03-06 21:20:17 +0100

    add popover and scss file

--- web/assets/js/app/building.js
+++ web/assets/js/app/building.js
@@ -3,21 +3,23 @@
   */
  define(['jquery', 'jqueryui', 'router', 'bootstrap'], function ($) {
     console.log('building module loaded');
-     $('.unlock-object').on('click', function(e){
+
+    $('.unlock-object').on('click', function(e){
          e.preventDefault();
          var id = $(this).data("id");
          var obj = $(this);
          $.get(Routing.generate('znieh_villagegame_public_unlock', {object : id}))
-         .done(function(data){
+         .done(function(data) {
            console.log('done');
            obj.children().attr("src", 'img/icons/accept.png');
          })
          .fail(function(e) {
            console.log('fail');
          });
-     });
-     $(document).ready(function(){
-         $('.game-object').tooltip();
+    });
+
+    $(document).ready(function(){
+         $('.game-object').popover();
          var form = $('#znieh_unitgamebundle_weapon').parent();
          $(form).hide();
          $( ".draggable" ).draggable({ cursor: "move", helper: "clone", revert: "invalid" });
@@ -36,13 +38,12 @@
                      $(form)[0].reset();
                  }
              });
-
          });
-         $( "#droppable" ).droppable({
-               drop: function( event, ui ) {
-                   console.log($('#znieh_unitgamebundle_weapon_parts_' + ui.draggable.data("id")));
-                   $('#znieh_unitgamebundle_weapon_parts_' + ui.draggable.data("id")).prop('checked', true);
-               }
+         $("#droppable").droppable({
+              drop: function( event, ui ) {
+                  console.log($('#znieh_unitgamebundle_weapon_parts_' + ui.draggable.data("id")));
+                  $('#znieh_unitgamebundle_weapon_parts_' + ui.draggable.data("id")).prop('checked', true);
+              }
          });
      });
  });
