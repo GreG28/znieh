2bcc37a745bcb7607d038d8fed4424fbd17b224a
GreG28 <gregoire.fouillard@gmail.com>
2014-03-10 14:41:13 +0100

    The match making is now working and its redirecting the player to page game and the the page has to ask for the information to gameserver ! @Darkou @Alfo !

--- src/Znieh/CoreGameBundle/Controller/DefaultController.php
+++ src/Znieh/CoreGameBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/game")
+     * @Route("/game", name="core_game")
      * @Template()
      * @Security("has_role('ROLE_USER')")
      */
