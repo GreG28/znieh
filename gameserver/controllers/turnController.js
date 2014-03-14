7123bd40292a52ede200e104bfb65bd8a267d8db
Naixela <alexianfm@gmail.com>
2014-03-14 11:06:15 +0100

    turnController fixed

--- gameserver/controllers/turnController.js
+++ gameserver/controllers/turnController.js
@@ -1,34 +1,40 @@
 var turnController = {};
-module.exports = turnController;
 
-turnController.counter = 0;
-turnController.hasAttack = {};
-turnController.hasMove = {};
 
-turnController.newTurn = function (units){
-	this.counter++;
-
+turnController = function (){
+	this.counter = 0;
 	this.hasAttack = {};
 	this.hasMove = {};
 
-	for(var i in units){
-		this.hasAttack[units[i]] = false;
-		this.hasMove[units[i]] = false;
-	}
-}
 
-turnController.hasAttacked = function(unit){
-	return this.hasAttack[unit];
-}
+	this.newTurn = function (units){
+		this.counter++;
 
-turnController.hasMoved = function(unit){
-	return this.hasMoved[unit];
-}
+		this.hasAttack = {};
+		this.hasMove = {};
 
-turnController.setHasAttacked = function(unit){
-	this.hasAttack[unit] = true;
-}
+		for(var i in units){
+			this.hasAttack[units[i]] = false;
+			this.hasMove[units[i]] = false;
+		}
+	};
 
-turnController.setHasMoved = function(unit){
-	this.hasMove[unit] = true;
-}
+
+	this.hasAttacked = function(unit){
+		return this.hasAttack[unit];
+	};
+
+	this.hasMoved = function(unit){
+		return this.hasMoved[unit];
+	};
+
+	this.setHasAttacked = function(unit){
+		this.hasAttack[unit] = true;
+	};
+
+	this.setHasMoved = function(unit){
+		this.hasMove[unit] = true;
+	};
+
+};
+module.exports = turnController;
