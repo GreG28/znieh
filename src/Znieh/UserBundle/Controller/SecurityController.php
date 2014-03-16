04e249b96e55f8d037caaf7276025c7a3d3aa054
spyl94 <spyl94@gmail.com>
2014-03-16 15:50:22 +0100

    first part

--- src/Znieh/UserBundle/Controller/SecurityController.php
+++ src/Znieh/UserBundle/Controller/SecurityController.php
@@ -3,13 +3,49 @@
 namespace Znieh\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends BaseController
 {
 
-    protected function renderLogin(array $data)
+    public function loginAction(Request $request)
     {
-        $template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        return $this->renderLoginRoute(array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+        ), $request->attributes->get('_route'));
+    }
+
+    protected function renderLoginRoute(array $data, $route)
+    {
+        $templateName = $route == "fos_user_security_login" ? "login" : "login-form-content";
+        $template = sprintf('FOSUserBundle:Security:' . $templateName . '.html.%s', $this->container->getParameter('fos_user.template.engine'));
 
         $data['registration'] = $this->container->get('fos_user.registration.form.factory')->createForm()->createView();
 
