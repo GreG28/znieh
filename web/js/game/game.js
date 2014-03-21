fee6d563c85a413a376886adc085331afb17ce7f
Florian <florian.lepot@gmail.com>
2014-03-21 23:18:22 +0100

    First implementation of ennemy side

--- web/js/game/game.js
+++ web/js/game/game.js
@@ -58,14 +58,15 @@ function getSide() {
 
 function getUnits() {
   socket.emit('get-units', null, function(data) {
+    var taille;
     units = data;
     console.log('get-units ');
 
     units = data[0];
-    var taille;
 
     for(var i = 0 ; i < units.length ; i++)
     {
+      units[i].life = 100; // TODO : DELETE THIS LINE
       taille = "petitfin";
       if(units[i].size == "Normal" && units[i].weight == "Musclé")
       {
@@ -73,29 +74,31 @@ function getUnits() {
       }
     }
 
-    ennemy_units = data[1];
-    
-    for(var i = 0 ; i < ennemy_units.length ; i++)
+    ennemyUnits = data[1];
+
+    for(var i = 0 ; i < ennemyUnits.length ; i++)
     {
       taille = "petitfin";
-      if(ennemy_units[i].size == "Normal" && ennemy_units[i].weight == "Musclé")
+      if(ennemyUnits[i].size == "Normal" && ennemyUnits[i].weight == "Musclé")
       {
-          ennemy_units[i].taille = "petitfin";
+          ennemyUnits[i].taille = "petitfin";
       }
     }
 
+    units[0].statut = 0;
 
     continueProcess++;
     if(continueProcess == 3)
     {
       init();
     }
+
   });
 }
 
 var canvas = document.getElementById("canvas");
 var stage = new createjs.Stage(canvas);
-var ennemy_units = null;
+var ennemyUnits = null;
 var infoSide = null;
 var nextUnitID = 0;
 var units = null;
@@ -113,7 +116,7 @@ function init() {
 }
 
 mySide = '<h2>Mes unités</h2><div id="myUnits"></div>';
-ennemySide = '<h2>Unités ennemies</h2> <div id="ennemyUnits></div>';
+ennemySide = '<h2>Unités ennemies</h2><div id="ennemyUnits"></div>';
 
 function setMyUnitsSide() {
   if(left) {
@@ -124,6 +127,7 @@ function setMyUnitsSide() {
   }
 
   for (var i = units.length - 1; i >= 0; i--) {
+    console.log(units[i]);
     $("#myUnits").prepend('<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game=""><div class="col-sm-12"><div class="row"><div class="col-sm-1"><div class="portrait"><img id="unit-' + i + '-img" /></div></div><div class="col-sm-4"><span class="name">' + units[i].name + '</span></div><div class="col-sm-6"><div class="progress life"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + units[i].life + '%"></div></div></div></div></div><div class="row"><div class="col-sm-5 col-sm-offset-1 stats"><img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54 </div><div class="col-sm-6 skills"><img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque"></div></div></div><div class="clearfix"></div>');
     $("#unit-" + i).on("click", {id: i}, function(event) {
       if(gameStatut == GameStatut.PLACEMENT) {
@@ -153,7 +157,7 @@ function setMyUnitsSide() {
     });
 
     $("#unit-" + i).on("mouseover", {id: i}, function(event) {
-      setInfoSide(ContentManager.units[event.data.id]);
+      setInfoSide(units[event.data.id]);
     });
 
 
@@ -168,7 +172,7 @@ function setMyUnitsSide() {
 }
 
 function setInfoSide(data) {
-  infoSide = '<h2>&nbsp;</h2> <div id="infosUnit"><div class="row infos"><div class="col-sm-12" style="text-align:center;"><h3>Jacky</h3><hr>';
+  infoSide = '<h2>&nbsp;</h2> <div id="infosUnit"><div class="row infos"><div class="col-sm-12" style="text-align:center;"><h3>' + data.name + '</h3><hr>';
   infoSide += '<h4>Statistiques</h4><div class="stats"><div class="row">';
   infoSide += '<div class="col-sm-3"><img src="../img/icons/vie.png" title="Vie"> 67</div><div class="col-sm-3"><img src="../img/icons/penetration.png" title="Pénétration"> 67</div><div class="col-sm-3"><img src="../img/icons/precision.png" title="Précision"> 67</div><div class="col-sm-3"><img src="../img/icons/esquive.png" title="Esquive"> 67</div>';
   infoSide += '<div class="col-sm-3"><img src="../img/icons/parade.png" title="Parade"> 67</div><div class="col-sm-3"><img src="../img/icons/defense.png" title="Défense"> 67</div><div class="col-sm-3"><img src="../img/icons/force.png" title="Force"> 67</div><div class="col-sm-3"><img src="../img/icons/agilite.png" title="Agilité"> 67</div><div class="col-sm-3 col-sm-offset-3"><img src="../img/icons/intelligence.png" title="Intelligence"> 67</div><div class="col-sm-3"><img src="../img/icons/armure.png" title="Armure"> 67</div></div>';
@@ -190,5 +194,9 @@ function setEnnemySide() {
   }
   else {
     $("#leftSide").html(ennemySide);
+  }
+
+  for (var i = ennemyUnits.length - 1; i >= 0; i--) {
+    $("#ennemyUnits").prepend('<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game=""><div class="col-sm-12"><div class="row"><div class="col-sm-1"><div class="portrait"><img id="unit-' + i + '-img" /></div></div><div class="col-sm-4"><span class="name">' + ennemyUnits[i].name + '</span></div><div class="col-sm-6"><div class="progress life"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + ennemyUnits[i].life + '%"></div></div></div></div></div><div class="row"><div class="col-sm-5 col-sm-offset-1 stats"><img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54 </div><div class="col-sm-6 skills"><img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque"></div></div></div><div class="clearfix"></div>');
   }
 }
