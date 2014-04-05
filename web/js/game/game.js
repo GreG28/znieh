762f469eb9147201c29780c3d0b4908e500cfbeb
Florian <florian.lepot@gmail.com>
2014-04-05 20:52:04 +0200

    Feat slide menu

--- web/js/game/game.js
+++ web/js/game/game.js
@@ -80,6 +80,7 @@ function getUnits() {
 
     for(i = 0 ; i < ennemyUnits.length ; i++)
     {
+      ennemyUnits[i].life = 100; // TODO : DELETE THIS LINE
       taille = "petitfin";
       if(ennemyUnits[i].size == "Normal" && ennemyUnits[i].weight == "Musclé")
       {
@@ -136,7 +137,9 @@ function setMyUnitsSide() {
 
   for (var i = units.length - 1; i >= 0; i--) {
     console.log(units[i]);
-    $("#myUnits").prepend('<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game=""><div class="col-sm-12"><div class="row"><div class="col-sm-1"><div class="portrait"><img id="unit-' + i + '-img" /></div></div><div class="col-sm-4"><span class="name">' + units[i].name + '</span></div><div class="col-sm-6"><div class="progress life"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + units[i].life + '%"></div></div></div></div></div><div class="row"><div class="col-sm-5 col-sm-offset-1 stats"><img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54 </div><div class="col-sm-6 skills"><img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque"></div></div></div><div class="clearfix"></div>');
+
+    $("#myUnits").prepend(getUnitsMenuRender(0, i, units[i], ContentManager.left));
+
     $("#myUnits #unit-" + i).on("click", {id: i}, function(event) {
       if(gameStatut == GameStatut.PLACEMENT) {
         if(units[event.data.id].statut == -1) {
@@ -199,11 +202,11 @@ function setInfoSide(data) {
 
     if(ContentManager.left) {
       ennemySideSauv = $("#rightSide").html();
-      $("#rightSide").html(infoSide);
+      //$("#rightSide").html(infoSide);
     }
     else {
       ennemySideSauv = $("#leftSide").html();
-      $("#leftSide").html(infoSide);
+      //$("#leftSide").html(infoSide);
     }
     flag = 1;
   }
@@ -228,7 +231,7 @@ function setEnnemySide() {
       }
 
       for (var i = ennemyUnits.length - 1; i >= 0; i--) {
-        $("#ennemyUnits").prepend('<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game=""><div class="col-sm-12"><div class="row"><div class="col-sm-1"><div class="portrait"><img id="unit-' + i + '-img" /></div></div><div class="col-sm-4"><span class="name">' + ennemyUnits[i].name + '</span></div><div class="col-sm-6"><div class="progress life"><div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + ennemyUnits[i].life + '%"></div></div></div></div></div><div class="row"><div class="col-sm-5 col-sm-offset-1 stats"><img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54 </div><div class="col-sm-6 skills"><img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque"></div></div></div><div class="clearfix"></div>');
+        $("#ennemyUnits").prepend(getUnitsMenuRender(1, i, ennemyUnits[i], ContentManager.left));
       }
     }
     else if(flag == 1)
@@ -247,6 +250,78 @@ function setEnnemySide() {
     {
       console.log("setEnnemySide() -> flag was false");
     }
+}
+
+function getUnitsMenuRender(type, i, data, left) { // Type : 0 (myUnits) | 1 (ennemyUnits)
+  var render;
+
+  if((left == true && type == 1) || (left == false && type == 0)) {
+    render = '<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game="">';
+      render +='  <div class="col-sm-12">';
+        render +='  <div class="row">';
+          render +='  <div class="col-sm-3">';
+            render +='  <div class="portrait">';
+              render +='  <img id="unit-' + i + '-img" />';
+            render +='  </div>';
+          render +='  </div>';
+          render +='  <div class="col-sm-5 stats">';
+            render +='  <img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54';
+          render +='  </div>';
+          render +='  <div class="col-sm-4">';
+            render +='  <div class="progress life">';
+              render +='  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + data.life + '%"></div>';
+            render +='  </div>';
+          render +='  </div>';
+        render +='  </div>';
+        render +='  <div class="row">';
+          render +='  <div class="col-sm-3">';
+            render +='  <div class="name">' + data.name + '</div>';
+          render +='  </div>';
+          render +='  <div class="col-sm-offset-1 col-sm-8">';
+            render +='  <div class="skills">';
+              render +='  <img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque">';
+            render +='  </div>';
+          render +='  </div>';
+        render +='  </div>';
+      render +='  </div>';
+    render +='  </div>';
+    render +='  <div class="clearfix"></div>';
+  }
+  else {
+    render = '<div class="row unit" id="unit-' + i + '" data-unit="' + i + '" data-unit-game="">';
+      render +='  <div class="col-sm-12">';
+        render +='  <div class="row">';
+          render +='  <div class="col-sm-4">';
+            render +='  <div class="progress life">';
+              render +='  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: ' + data.life + '%"></div>';
+            render +='  </div>';
+          render +='  </div>';
+          render +='  <div class="col-sm-5 stats">';
+            render +='  <img src="../img/icons/attack.png" alt="Attaque"> 67 <img src="../img/icons/defense.png" alt="Défense"> 35 <img src="../img/icons/precision.png" alt="Précision"> 54';
+          render +='  </div>';
+          render +='  <div class="col-sm-3">';
+            render +='  <div class="portrait">';
+              render +='  <img id="unit-' + i + '-img" />';
+            render +='  </div>';
+          render +='  </div>';
+        render +='  </div>';
+        render +='  <div class="row">';
+          render +='  <div class="col-sm-offset-1 col-sm-8">';
+            render +='  <div class="skills">';
+              render +='  <img src="../img/icons/spell0.png" alt="Attaque"><img src="../img/icons/spell1.png" alt="Attaque"><img src="../img/icons/spell2.png" alt="Attaque"><img src="../img/icons/spell3.png" alt="Attaque">';
+            render +='  </div>';
+          render +='  </div>';
+          render +='  <div class="col-sm-3">';
+            render +='  <div class="name">' + data.name + '</div>';
+          render +='  </div>';
+        render +='  </div>';
+      render +='  </div>';
+    render +='  </div>';
+    render +='  </div>';
+    render +='  <div class="clearfix"></div>';
+  }
+
+  return render;
 }
 
 function setSocketOnForGame()
